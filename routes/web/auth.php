b669dfe961f70c8f5a9307b5ba716cc171a8a236
anubra266 <anubra266@gmail.com>
2020-10-22 00:29:28 +0100

    refactor: remove kiddie info from routes

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Auth Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register auth routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 
 // Auth::routes();
 
--- routes/web/main.php
+++ routes/web/main.php
@@ -1,20 +1,9 @@
 <?php
 
 use App\Http\Requests\ContactMessage;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Home', [
@@ -32,8 +21,8 @@ Route::get('/contact', function () {
 })->name('contact');
 
 Route::post('/submit', function (ContactMessage $request) {
-    $message = $request->validated();
-    return redirect()->back()->with('success', 'Data accepted');
+    $message = (object)$request->validated();
+    return redirect()->back()->with('success', "Data accepted! $message->first_name");
 })->name('contact.message');
 
 Route::get('/blog', function () {
