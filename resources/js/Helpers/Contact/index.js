a6ba4a5059523737c540ff9cb1e65df188797ec7
anubra266 <anubra266@gmail.com>
2020-10-22 16:18:09 +0100

    fix: update right use of request events

--- resources/js/Helpers/Contact/index.js
+++ resources/js/Helpers/Contact/index.js
@@ -5,12 +5,12 @@ class ContactHelper {
         this.load = load;
     }
     submit(data) {
-        this.load(true);
         //*Inertia implements the custom methods
         Inertia.post(route("contact.message"), data, {
-            onStart: () => console.log("Sending"),
-            onFinish: () => console.log("Sent"),
-            onSuccess: () => this.load(false)
+            preserveScroll: true,
+            onStart: () => this.load(true),
+            onFinish: () => this.load(false),
+            onSuccess: () => console.log("Finished")
         });
     }
 }
