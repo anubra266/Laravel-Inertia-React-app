396501318a4e3640c6780a68bf4b50904baad642
anubra266 <anubra266@gmail.com>
2020-10-22 16:05:02 +0100

    fix: update Inertia new request syntax

--- resources/js/Helpers/Contact/index.js
+++ resources/js/Helpers/Contact/index.js
@@ -4,12 +4,14 @@ class ContactHelper {
     constructor(load) {
         this.load = load;
     }
-    submit(data) { 
+    submit(data) {
         this.load(true);
         //*Inertia implements the custom methods
-        Inertia.post(route("contact.message"), data).then(() =>
-            this.load(false)
-        );
+        Inertia.post(route("contact.message"), data, {
+            onStart: () => console.log("Sending"),
+            onFinish: () => console.log("Sent"),
+            onSuccess: () => this.load(false)
+        });
     }
 }
 export default ContactHelper;
