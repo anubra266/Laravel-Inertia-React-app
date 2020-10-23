8e4d074830405ea1e036d4ba4a91155265f34bb5
anubra266 <anubra266@gmail.com>
2020-10-23 04:35:47 +0100

    refactor: remove stale layout key

--- app/Traits/Routes/General.php
+++ app/Traits/Routes/General.php
@@ -14,31 +14,26 @@ trait General
             [
                 "name" => "Home",
                 "route" => "home",
-                "layout" => "general",
                 "show" => true
             ],
             [
                 "name" => "About",
                 "route" => "about",
-                "layout" => "general",
                 "show" => true
             ],
             [
                 "name" => "Contact",
                 "route" => "contact",
-                "layout" => "general",
                 "show" => true
             ],
             [
                 "name" => "Blog",
                 "route" => "blog",
-                "layout" => "general",
                 "show" => true
             ],
             [
                 "name" => "Propsy",
                 "route" => "propsy",
-                "layout" => "general",
                 "show" => true
             ],
         ];
