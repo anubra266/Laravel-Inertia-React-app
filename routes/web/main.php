85b6e923e71c41c07239f114cd2284c209d63ba0
anubra266 <anubra266@gmail.com>
2020-10-26 18:37:18 +0100

    refactor: hardly neccessary

--- routes/web/main.php
+++ routes/web/main.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return Inertia::render('Index', [
-        'users' => ''
-    ]);
+    return Inertia::render('Index', []);
 })->name('index');
 
 Route::get('/about', function () {
