dc8788c630273f17d206e87fac1d000a043dd294
anubra266 <anubra266@gmail.com>
2020-10-26 20:18:00 +0100

    fix: add paramters to visit helper

--- app/Helpers/Route.php
+++ app/Helpers/Route.php
@@ -15,12 +15,12 @@ if (!function_exists('backward')) {
 if (!function_exists('visit')) {
     /**
      * Redirect to any route
-     * @param string $route Named route to visit
+     * @param array $route Named route to visit, and it's parameters
      * @param string $status Status of Request Response
      * @param string $response The Request's response
      */
     function visit($route, $status = '', $response = '')
     {
-        return redirect()->route($route)->with($status, $response);
+        return redirect()->route(...$route)->with($status, $response);
     }
 }
