5bb0a5a9fb3c821bf302ffb3a358a7a84e2980d9
anubra266 <anubra266@gmail.com>
2020-11-05 14:16:38 +0100

    remove local hooks

--- resources/js/Hooks/index.js
+++ resources/js/Hooks/index.js
@@ -1,16 +1,1 @@
-export { useFCap, usePlural } from "./lib/string";
-export {
-    useDualRef,
-    useDynamicRefs,
-    useExpose,
-    useIprops,
-    useTitle,
-    useTheme,
-    useThemeVar,
-    useScroll,
-    useSmoothRefresh
-} from "./lib/app";
-export { useAsort, useShuffle } from "./lib/array";
-export { useAntDate, useDbDate, useHumanDate } from "./lib/datez";
 export { useFlashMessage } from "./lib/FlashMessage";
-export { useUrlQuery } from "./lib/url";
